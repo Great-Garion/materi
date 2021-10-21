688132882c2117400f278abb93f3c8e3d76103bc
thoriqnf <thoriqnfaizal@gmail.com>
2021-10-21 11:25:27 +0700

    Adding materi onchange dynamic react hooks

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -5,6 +5,11 @@ function App() {
   // membuat sebuah state
   const [name, setName] = useState("skilvul");
   const [number, setNumber] = useState(0);
+  const [inputName, setInputName] = useState("impact byte");
+  const [murid, setMurid] = useState({
+    nama: "",
+    kelas: 0,
+  });
 
   // membuat variable
   const nickName = "thoriq";
@@ -14,7 +19,25 @@ function App() {
     // if (number < 10) {
     //   setNumber(number + 1);
     // }
-    // number < 10 ? setNumber(number + 1) : null;
+    // eslint-disable-next-line no-unused-expressions
+    number < 10 ? setNumber(number + 1) : null;
+  };
+
+  const handleChangeInput = (event) => {
+    // console.log(event);
+    setInputName(event.target.value);
+  };
+
+  const handleChangeMurid = (event) => {
+    // console.log(event.target.name);
+    const { name, value } = event.target;
+
+    setMurid((prevMurid) => ({
+      ...prevMurid,
+      [name]: value,
+      // [event.target.name]: event.target.value,
+    }));
+    console.log(murid);
   };
 
   return (
@@ -35,6 +58,25 @@ function App() {
         <button onClick={() => (number > 0 ? setNumber(number - 1) : null)}>
           -
         </button>
+      </div>
+      <div>
+        <input type="text" value={inputName} onChange={handleChangeInput} />
+      </div>
+      <h1>{inputName}</h1>
+
+      <div>
+        <input
+          type="text"
+          value={murid.nama}
+          name="nama"
+          onChange={handleChangeMurid}
+        />
+        <input
+          type="number"
+          value={murid.kelas}
+          name="kelas"
+          onChange={handleChangeMurid}
+        />
       </div>
     </div>
   );
